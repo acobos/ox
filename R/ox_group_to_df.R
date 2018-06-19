f2108c665d379b4f8d2c74c27456c03b5e9d27f5
Albert Cobos <acoboscarbo@gmail.com>
2018-06-19 19:07:32 +0200

    New version of function ox_group_to_df(), to get variables in order as defined in item_ref

--- R/ox_group_to_df.R
+++ R/ox_group_to_df.R
@@ -7,12 +7,13 @@
 #'
 #' @examples
 #'
-ox_group_to_df <- function (item_data, GroupOID) {
+ox_group_to_df <- function (GroupOID, item_data, item_ref) {
 
   # IMPROVEMENTS:
-  # - nicer varnames: item_name (from item_def)
   # - factor definitions from codelists
+  # - nicer varnames: item_name (from item_def)
 
+  # to get the data
   item_data %>%
     filter(group_oid == GroupOID) %>%
     select(study_oid,
@@ -23,13 +24,43 @@ ox_group_to_df <- function (item_data, GroupOID) {
            item_oid,
            group_repeat_key,
            value) %>%
-    tidyr::spread(item_oid , value)
+    tidyr::spread(item_oid , value) -> k
+
+  # to define the var order of non-key vars
+  item_ref %>%
+    filter(group_oid == GroupOID) %>%
+    arrange(item_order_number) %>%
+    pull(item_oid) -> vars_in_order
+
+  # to identify key vars
+  key_vars <- names(k)[!(names(k) %in% vars_in_order)]
+
+  # output
+  k %>%
+    select(one_of(key_vars), one_of(vars_in_order))
+
 }
 
-# # To define factors
+# # verify: ----
+# k <- ox_group_to_df("IG_CANCE_CANCERHISTOLOGYANDRECEPTOR",
+#                     item_data,
+#                     item_ref)
+# dat <- item_data; # rm(item_data)
+# items_reference <- item_ref; # rm(item_ref)
+# kk <- ox_group_to_df("IG_CANCE_CANCERHISTOLOGYANDRECEPTOR",
+#                      dat,
+#                      items_reference)
+# k <- ox_group_to_df("IG_IECRI_IECRITERIA",
+#                     dat,
+#                     items_reference)
+# rm(dat, items_reference, k, kk)
+# k <- ox_group_to_df("IG_CANCE_CANCERHISTOLOGYANDRECEPTOR", item_data, item_ref)
+
+
+# # To define factors ----
 #
-# # identify vars with codelist
-# ox_group_to_df(item_data, "IG_V1D01_UNGROUPED") -> g
+# identify vars with codelist
+# ox_group_to_df("IG_V1D01_UNGROUPED") -> g
 #
 #
 # vars <- names(g)[names(g) %in% cl_r$item_oid]
