74e3ead6c2a71b3debf6290b08eb01d1f3ee9707
Albert Cobos <acoboscarbo@gmail.com>
2018-10-30 17:26:27 +0100

    Bug fix, to always get a data.frame

--- R/ox_subject_data.R
+++ R/ox_subject_data.R
@@ -9,19 +9,11 @@
 #'
 ox_subject_data <- function (parsed_xml) {
 
-  sd <- xpathSApply(doc, "//ns:ClinicalData/ns:SubjectData",
-                    namespaces=ox_alias_default_ns(parsed_xml),
-                    fun=xmlAttrs)
+  sd <- xpathApply(parsed_xml, "//ns:ClinicalData/ns:SubjectData",
+                   namespaces=ox_alias_default_ns(doc),
+                   fun=xmlAttrs)
 
-  # function to get a df for each element in sd
-  to_df <- function (x) {
-    attr(x, "namespaces") <- NULL   # delete namspace attribute
-    t(as.data.frame(x)) -> x_df     # as df
-    names(x_df) <- attr(x, "names") # because names are lost
-    row.names(x_df) <- NULL         # avoid rownames
-    as.data.frame(x_df, stringsAsFactors=FALSE) # again, as df
-  }
-
-  bind_rows(lapply(sd, to_df))   # apply function to all list elements
+  as.data.frame(do.call(rbind, sd),
+                stringsAsFactors = FALSE)
 }
 
