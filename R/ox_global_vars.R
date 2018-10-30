4e297a9ca16800cf2e52509790a0c2f7d23f7599
Albert Cobos <acoboscarbo@gmail.com>
2018-10-30 19:29:50 +0100

    Better not use sapply (type unstable)

--- R/ox_global_vars.R
+++ R/ox_global_vars.R
@@ -8,7 +8,7 @@
 #' @examples
 #'
 ox_global_vars <- function (parsed_xml) {
-  as.list(sapply(xpathApply(parsed_xml,
+  as.list(lapply(xpathApply(parsed_xml,
                             "//ns:Study/ns:GlobalVariables",
                             namespaces = ox_alias_default_ns(parsed_xml),
                             fun=xmlChildren)[[1]],
