62970ef43c8beba4e48ef6afebb5c9a679c8b122
Albert Cobos <acoboscarbo@gmail.com>
2018-10-31 20:16:17 +0100

    Forget about encoding: arg name_encoded dropped, and all reference to encoding in documentation was eliminated

--- R/ox_event_def.R
+++ R/ox_event_def.R
@@ -1,8 +1,7 @@
 #' Gets OpenClinica (OC) study event definitions
 #'
 #' Returns a dataframe with study event definitions from a parsed OC .xml
-#' export file, provided as argument. Allows to define the character encoding
-#' for event names.
+#' export file, provided as argument.
 #'
 #' @param parsed_xml An object of class \code{XMLInternalDocument}, as returned
 #' by \code{XML::xmlParse()}.
@@ -19,34 +18,21 @@
 #'
 #' # Parsing the xml file
 #' library(XML)
-#' doc <- xmlParse(xmlFile, encoding="US_ASCII")
+#' doc <- xmlParse(xmlFile)
 #'
 #' # get event definitions
 #'ox_event_def(doc)
 #'
-#'# same, defining encoding
-#'ox_event_def(doc, "latin-1")
 #'}
 #'
-ox_event_def <- function (parsed_xml, name_encoding="UTF-8") {
+ox_event_def <- function (parsed_xml) {
 
   if (! "XMLInternalDocument" %in% class(parsed_xml)) {
     stop("parsed_xml should be an object of class XMLInternalDocument", call. = FALSE)
   }
 
-  if (class(name_encoding) != "character") {
-    stop("name_encoding should be a character vector of length 1", call. = FALSE)
-  }
-
-  if (length(name_encoding) > 1) {
-    stop("name_encoding should be a character vector of length 1", call. = FALSE)
-  }
-
-  if (!name_encoding %in% c("UTF-8", "latin-1")) {
-    stop("name_encoding should be either `UTF-8` or `latin-1`", call. = FALSE)
-  }
-
-  ev_def <- bind_rows(lapply(xpathApply(parsed_xml,
+  # return
+  bind_rows(lapply(xpathApply(parsed_xml,
                               "//ns:StudyEventDef",
                               namespaces = ox_alias_default_ns(parsed_xml),
                               fun=xmlAncestors,
@@ -59,9 +45,5 @@ ox_event_def <- function (parsed_xml, name_encoding="UTF-8") {
            event_name = Name.1,
            event_repeating = Repeating,
            event_type = Type)
-
-  Encoding(ev_def$event_name) <- name_encoding
-
-  ev_def
 
 }
