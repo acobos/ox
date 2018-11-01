a6221c63040fbbbb6097c8be0164e2d1ba437f0c
Albert Cobos <acoboscarbo@gmail.com>
2018-11-01 18:35:52 +0100

    Drop encoding parameter + documentation

--- R/ox_sites.R
+++ R/ox_sites.R
@@ -1,18 +1,33 @@
-#' Extracts sites from a parsed OpenClinica xml file
+#' Sites in a dataframe
 #'
-#' @param parsed_xml an object of class XMLInternalDocument
-#' @param encoding
+#' Returns a dataframe with study sites from a parsed OpenClinica
+#' odm1.3 .xml export file.
 #'
-#' @return dataframe with study_oid and site (names)
+#' @param parsed_xml An object of class \code{XMLInternalDocument}, as returned
+#' by \code{XML::xmlParse()}.
+#'
+#' @return A dataframe.
 #' @export
 #'
 #' @examples
+#' # The example xml file address
+#' file <- system.file("extdata",
+#'                     "odm1.3_clinical_ext_example.xml",
+#'                     package = "ox",
+#'                     mustWork = TRUE)
 #'
-ox_sites <- function (parsed_xml, encoding="UTF-8"){
+#' # Parsing the xml file
+#' library(XML)
+#' doc <- xmlParse(file_address)
+#'
+#' # Sites in a dataframe
+#' sites <- ox_sites(doc)
+#' View(sites)
+#'
+ox_sites <- function (parsed_xml){
   s <- as.character(xpathApply(parsed_xml, "//ns:StudyName",
                                namespaces=ox_alias_default_ns(doc),
                                fun=xmlValue))
-  Encoding(s) <- encoding
 
   # reading Study OIDs
   o <- xpathApply(parsed_xml, "//ns:Study",
