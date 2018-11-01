6a60a5ec859f68c943b41d18d468ca80945baf64
Albert Cobos <acoboscarbo@gmail.com>
2018-11-01 07:28:05 +0100

    Documentation

--- R/ox_form_def.R
+++ R/ox_form_def.R
@@ -1,13 +1,35 @@
-#' Title Form definitions
+#' Form definitions in a dataframe
 #'
-#' @param parsed_xml an object of class XMLInternalDocument
+#' Returns a dataframe with study form definitions from a parsed OpenClinica
+#' odm1.3 .xml export file.
 #'
-#' @return dataframe
+#' @param parsed_xml An object of class \code{XMLInternalDocument}, as returned
+#' by \code{XML::xmlParse()}.
+#'
+#' @return A dataframe.
 #' @export
 #'
 #' @examples
+#' # The example xml file address
+#' file <- system.file("extdata",
+#'                     "odm1.3_clinical_ext_example.xml",
+#'                     package = "ox",
+#'                     mustWork = TRUE)
 #'
+#' # Parsing the xml file
+#' library(XML)
+#' doc <- xmlParse(file_address)
+#'
+#' # Form definitions in a dataframe
+#' form_def <- ox_form_def(doc)
+#' View(event_def)
 ox_form_def <- function (parsed_xml) {
+
+  if (! "XMLInternalDocument" %in% class(parsed_xml)) {
+    stop("parsed_xml should be an object of class XMLInternalDocument", call. = FALSE)
+  }
+
+  # return
   bind_rows(lapply(xpathApply(parsed_xml,
                               "//ns:FormDef",
                               namespaces = ox_alias_default_ns(parsed_xml),
