464bcf6a98e4a56dc38878127692a26c85ca03a7
Albert Cobos <acoboscarbo@gmail.com>
2018-11-01 12:36:25 +0100

    Documentation

--- R/ox_group_def.R
+++ R/ox_group_def.R
@@ -1,14 +1,35 @@
-#' Title ItemGroup definitions
+#' Group definitions in a dataframe
 #'
-#' @param parsed_xml an object of class XMLInternalDocument
+#' Returns a dataframe with study item group definitions from a parsed OpenClinica
+#' odm1.3 .xml export file.
 #'
-#' @return dataframe
+#' @param parsed_xml An object of class \code{XMLInternalDocument}, as returned
+#' by \code{XML::xmlParse()}.
+#'
+#' @return A dataframe.
 #' @export
 #'
 #' @examples
+#' # The example xml file address
+#' file <- system.file("extdata",
+#'                     "odm1.3_clinical_ext_example.xml",
+#'                     package = "ox",
+#'                     mustWork = TRUE)
 #'
+#' # Parsing the xml file
+#' library(XML)
+#' doc <- xmlParse(file_address)
+#'
+#' # Event definitions in a dataframe
+#' group_def <- ox_group_def(doc)
+#' View(group_def)
 ox_group_def <- function (parsed_xml) {
 
+  if (! "XMLInternalDocument" %in% class(parsed_xml)) {
+    stop("parsed_xml should be an object of class XMLInternalDocument", call. = FALSE)
+  }
+
+  # return
   bind_rows(lapply(xpathApply(parsed_xml,
                               "//ns:ItemGroupDef",
                               namespaces = ox_alias_default_ns(parsed_xml),
