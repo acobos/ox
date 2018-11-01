fc3bdcaf04bdd33eb60593bfd9dc24781a386ed9
Albert Cobos <acoboscarbo@gmail.com>
2018-11-01 19:29:18 +0100

    Documentation

--- R/ox_audit_log.R
+++ R/ox_audit_log.R
@@ -1,12 +1,28 @@
-#' Extracts the Audit log from an ox object
+#' Audit log in a dataframe
 #'
-#' @param ox_obj An object created with ox()
+#' Returns a dataframe with study audit log from a parsed OpenClinica
+#' odm1.3_full .xml export file.
 #'
-#' @return dataframe containing Audit log entries
+#' @param parsed_xml An object of class \code{XMLInternalDocument}, as returned
+#' by \code{XML::xmlParse()}.
+#'
+#' @return A dataframe.
 #' @export
 #'
 #' @examples
+#' # The example xml file address
+#' file <- system.file("extdata",
+#'                     "odm1.3_clinical_ext_example.xml",
+#'                     package = "ox",
+#'                     mustWork = TRUE)
 #'
+#' # Parsing the xml file
+#' library(XML)
+#' doc <- xmlParse(file_address)
+#'
+#' # Audit log in a dataframe
+#' audit_log <- ox_audit_log(doc)
+#' View(audit_log)
 ox_audit_log <- function (ox_obj) {
 
   OpenClinica.ns <- c(OpenClinica=xmlNamespaces(doc)$OpenClinica$uri)
