9c35ce976ba24e6e0d731010c9d9804043de7b8a
Albert Cobos <acoboscarbo@gmail.com>
2018-11-01 19:04:55 +0100

    change function call by res

--- tests/test_form_def.R
+++ tests/test_form_def.R
@@ -19,14 +19,14 @@ res <- ox_form_def(doc)
 test_that("returns dataframe with expected variables, at least 1 row", {
   expect_is(res, "data.frame")
   # vars
-  expect_true("study_oid" %in% names(ox_form_def(doc)))
-  expect_true("version" %in% names(ox_form_def(doc)))
-  expect_true("metadata_version" %in% names(ox_form_def(doc)))
-  expect_true("form_oid" %in% names(ox_form_def(doc)))
-  expect_true("form_name" %in% names(ox_form_def(doc)))
-  expect_true("form_repeating" %in% names(ox_form_def(doc)))
+  expect_true("study_oid" %in% names(res))
+  expect_true("version" %in% names(res))
+  expect_true("metadata_version" %in% names(res))
+  expect_true("form_oid" %in% names(res))
+  expect_true("form_name" %in% names(res))
+  expect_true("form_repeating" %in% names(res))
   # rows
-  expect_true(nrow(ox_form_def(doc)) >= 1)
+  expect_true(nrow(res) >= 1)
 })
 
 test_that("dataframe has no factors", {
