434b1e4e37569dd859fd0e7f909bb7e9741e3cd2
Albert Cobos <acoboscarbo@gmail.com>
2018-11-01 07:28:19 +0100

    Tests

--- tests/test_events_def.R
+++ tests/test_events_def.R
@@ -13,21 +13,30 @@ library(ox)
 
 library(testthat)
 
-vars <- c("study_oid", "version", "metadata_version",
-          "event_oid", "event_name", "event_repeating", "event_type")
+# correct call
+res <- ox_event_def(doc)
 
-test_that("returns data frame with vars variables", {
-  expect_is(ox_event_def(doc), "data.frame")
-  expect_true("study_oid" %in% names(ox_event_def(doc)))
-  expect_true("version" %in% names(ox_event_def(doc)))
-  expect_true("metadata_version" %in% names(ox_event_def(doc)))
-  expect_true("event_oid" %in% names(ox_event_def(doc)))
-  expect_true("event_name" %in% names(ox_event_def(doc)))
-  expect_true("event_repeating" %in% names(ox_event_def(doc)))
-  expect_true("event_type" %in% names(ox_event_def(doc)))
-  expect_true(nrow(ox_event_def(doc)) >= 1)
+test_that("returns dataframe with expected variables, at least 1 row", {
+  expect_is(res, "data.frame")
+  # vars
+  expect_true("study_oid" %in% names(res))
+  expect_true("version" %in% names(res))
+  expect_true("metadata_version" %in% names(res))
+  expect_true("event_oid" %in% names(res))
+  expect_true("event_name" %in% names(res))
+  expect_true("event_repeating" %in% names(res))
+  expect_true("event_type" %in% names(res))
+  # rows
+  expect_true(nrow(res) >= 1)
+})
+
+test_that("dataframe has no factors", {
+  expect_false("factor" %in% unique(sapply(res, class)))
 })
 
 test_that("gives error when arg is not of expected class", {
   expect_error(ox_event_def(file))
 })
+
+# clean
+rm(doc, file, res)
