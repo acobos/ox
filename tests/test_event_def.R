7c62416d325be60d780d600d8c678520311df76b
Albert Cobos <acoboscarbo@gmail.com>
2018-11-01 12:38:05 +0100

    Documentation

---
+++ tests/test_event_def.R
@@ -0,0 +1,42 @@
+context("Event definitions")
+
+# getting the file address
+file <- system.file("extdata", "odm1.3_clinical_ext_example.xml",
+                    package = "ox",
+                    mustWork = TRUE)
+# Parsing the xml file
+library(XML)
+doc <- xmlParse(file)
+
+library(dplyr)
+library(ox)
+
+library(testthat)
+
+# correct call
+res <- ox_event_def(doc)
+
+test_that("returns dataframe with expected variables, at least 1 row", {
+  expect_is(res, "data.frame")
+  # vars
+  expect_true("study_oid" %in% names(res))
+  expect_true("version" %in% names(res))
+  expect_true("metadata_version" %in% names(res))
+  expect_true("event_oid" %in% names(res))
+  expect_true("event_name" %in% names(res))
+  expect_true("event_repeating" %in% names(res))
+  expect_true("event_type" %in% names(res))
+  # rows
+  expect_true(nrow(res) >= 1)
+})
+
+test_that("dataframe has no factors", {
+  expect_false("factor" %in% unique(sapply(res, class)))
+})
+
+test_that("gives error when arg is not of expected class", {
+  expect_error(ox_event_def(file))
+})
+
+# clean
+rm(doc, file, res)
