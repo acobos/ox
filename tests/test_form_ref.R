155b781d1d437fd7608ae0eedeaf0143358d1ec3
Albert Cobos <acoboscarbo@gmail.com>
2018-11-03 08:54:21 +0100

    Documentation

---
+++ tests/test_form_ref.R
@@ -0,0 +1,47 @@
+context("Form references")
+
+# getting the file address
+file <- system.file("extdata", "odm1.3_clinical_ext_example.xml",
+                    package = "ox",
+                    mustWork = TRUE)
+# Parsing the xml file
+library(XML)
+doc <- xmlParse(file)
+
+library(dplyr)
+library(ox)
+
+library(testthat)
+
+# incorrect call
+test_that("gives error when arg is not of expected class", {
+  expect_error(ox_form_ref(file))
+})
+
+# correct call
+res <- ox_form_ref(doc)
+
+test_that("returns dataframe with expected variables, at least 1 row", {
+  expect_is(res, "data.frame")
+  # vars
+  expect_true("study_oid" %in% names(res))
+  expect_true("version" %in% names(res))
+  expect_true("metadata_version" %in% names(res))
+  expect_true("event_oid" %in% names(res))
+  expect_true("event_name" %in% names(res))
+  expect_true("event_repeating" %in% names(res))
+  expect_true("event_type" %in% names(res))
+  expect_true("form_oid" %in% names(res))
+  expect_true("form_mandatory" %in% names(res))
+  # rows
+  expect_true(nrow(res) >= 1)
+})
+
+test_that("dataframe has no factors", {
+  expect_false("factor" %in% unique(sapply(res, class)))
+})
+
+
+# clean
+rm(doc, file, res)
+
---
+++ tests/test_item_ref.R
@@ -0,0 +1,47 @@
+context("Item references")
+
+# getting the file address
+file <- system.file("extdata", "odm1.3_clinical_ext_example.xml",
+                    package = "ox",
+                    mustWork = TRUE)
+# Parsing the xml file
+library(XML)
+doc <- xmlParse(file)
+
+library(dplyr)
+library(ox)
+
+library(testthat)
+
+# incorrect call
+test_that("gives error when arg is not of expected class", {
+  expect_error(ox_item_ref(file))
+})
+
+# correct call
+res <- ox_item_ref(doc)
+
+test_that("returns dataframe with expected variables, at least 1 row", {
+  expect_is(res, "data.frame")
+  # vars
+  expect_true("study_oid" %in% names(res))
+  expect_true("version" %in% names(res))
+  expect_true("metadata_version" %in% names(res))
+  expect_true("group_oid" %in% names(res))
+  expect_true("group_name" %in% names(res))
+  expect_true("group_repeating" %in% names(res))
+  expect_true("sas_dataset_name" %in% names(res))
+  expect_true("item_oid" %in% names(res))
+  expect_true("item_order_number" %in% names(res))
+  expect_true("item_mandatory" %in% names(res))
+  # rows
+  expect_true(nrow(res) >= 1)
+})
+
+test_that("dataframe has no factors", {
+  expect_false("factor" %in% unique(sapply(res, class)))
+})
+
+# clean
+rm(doc, file, res)
+
