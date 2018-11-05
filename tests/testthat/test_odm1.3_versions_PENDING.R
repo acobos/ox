1f87171e4570b0f59c2e4537bfa06580e103ccf5
Albert Cobos <acoboscarbo@gmail.com>
2018-11-05 14:09:36 +0100

    Test odm1.3full

--- tests/testthat/test_odm1.3_versions_PENDING.R
+++ tests/testthat/test_odm1.3_versions_PENDING.R
@@ -1,9 +1,16 @@
-context("Extract groups from ox object")
+context("odm1.3_clinical_ext")
 
 # getting the file address
-file <- system.file("extdata", "odm1.3_clinical_ext_example.xml",
-                    package = "ox",
-                    mustWork = TRUE)
+# file <- system.file("extdata", "odm1.3_clinical_ext_example.xml",
+#                     package = "ox",
+#                     mustWork = TRUE)
+
+dir <- "C:/Users/Administrador/Documents/My Dropbox/Common/SOPs/FSS/R/DM OpenClinica/ox testing"
+file <- paste(dir,
+              "odm1.3_clinical_ext_Prueba_todos_2018-11-05-124602172.xml",
+              sep="/")
+
+
 # Parsing the xml file
 library(XML)
 doc <- xmlParse(file)
@@ -16,34 +23,24 @@ d <- ox(doc)
 
 library(testthat)
 
-# correct call
-res <- ox_xtract_group(d, "IG_ACUTE_UNGROUPED")
-
 # incorrect call
-test_that("gives error when arg is not of expected class", {
-  expect_error(ox_xtract_group(doc, "IG_ACUTE_UNGROUPED"))
-  expect_error(ox_xtract_group(d))
-  expect_error(ox_xtract_group(d, doc))
-  expect_error(ox_xtract_group(d, "k"))
-  expect_error(ox_xtract_group(d, TRUE, "k"))
-})
 
 test_that("returns dataframe with expected variables, at least 1 row", {
-  expect_is(res, "data.frame")
-  # vars
-  expect_true("study_oid" %in% names(res))
-  expect_true("subject_id" %in% names(res))
-  expect_true("event_oid" %in% names(res))
-  expect_true("form_oid" %in% names(res))
-  expect_true("group_oid" %in% names(res))
-  expect_true("group_repeat_key" %in% names(res))
-
-  expect_is(res$study_oid, "character")
-  expect_is(res$subject_id, "character")
-  expect_is(res$event_oid, "character")
-  expect_is(res$form_oid, "character")
-  expect_is(res$group_oid, "character")
-  expect_is(res$group_repeat_key, "numeric")
+  expect_is(d$data, "data.frame")
+  expect_is(d$metadata$event_def, "data.frame")
+  expect_is(d$metadata$event_ref, "data.frame")
+  expect_is(d$metadata$form_def, "data.frame")
+  expect_is(d$metadata$form_ref, "data.frame")
+  expect_is(d$metadata$group_def, "data.frame")
+  expect_is(d$metadata$group_ref, "data.frame")
+  expect_is(d$metadata$item_def, "data.frame")
+  expect_is(d$metadata$item_ref, "data.frame")
+  expect_is(d$metadata$codelist, "data.frame")
+  expect_is(d$metadata$codelist_item, "data.frame")
+  expect_is(d$metadata$codelist_ref, "data.frame")
+  expect_is(d$metadata$units, "data.frame")
+  expect_is(d$metadata$sites, "data.frame")
+  expect_is(d$metadata$subjects, "data.frame")
 })
 
 # clean
