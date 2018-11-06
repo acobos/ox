ea3fd5dab6e6f9e4906459bea9a46fc8632d6431
Albert Cobos <acoboscarbo@gmail.com>
2018-11-06 22:57:58 +0100

    To provide more info.

--- R/summary.ox.R
+++ R/summary.ox.R
@@ -1,14 +1,20 @@
 # S3 method for ox objects
 
-summary.ox <- function (ox_obj, form_info = TRUE) {
-  ox_obj$data %>%
-    dplyr::select(form_oid, form_version, group_oid, event_oid) %>%
-    dplyr::arrange(form_oid, form_version, group_oid, event_oid) %>%
-    unique() %>%
-    dplyr::mutate(`form_oid, form_version, group_oid` = paste(form_oid, form_version, group_oid, sep=", ")) -> k
-  if (form_info == TRUE) {
-    with(k, table(`form_oid, form_version, group_oid`, event_oid))
-  } else {
-    with(k, table(`group_oid`, event_oid))
-  }
+summary.ox <- function (ox_obj) {
+
+  list(numbers = c(datapoints = nrow(d$data),
+                   subjects = length(unique(d$data$subject_key)),
+                   sites = length(unique(d$data$study_oid)),
+                   events = length(unique(d$data$event_oid)),
+                   forms = length(unique(d$data$form_oid)),
+                   groups = length(unique(d$data$group_oid)),
+                   items = length(unique(d$data$item_oid))),
+
+       events = unique(d$data$event_oid),
+       forms = unique(d$data$form_oid),
+       groups = unique(d$data$group_oid),
+       assessments = with(d$data, table(group_oid, event_oid))
+  )
 }
+
+
