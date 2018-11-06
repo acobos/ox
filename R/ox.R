460f3ec5ce48bf3d9882da553561469a5e27c4b4
Albert Cobos <acoboscarbo@gmail.com>
2018-11-06 11:40:46 +0100

    Simplify using new ox_metadata() fun.

--- R/ox.R
+++ R/ox.R
@@ -50,22 +50,8 @@ ox <- function (parsed_xml) {
     stop("parsed_xml should be an object of class XMLInternalDocument", call. = FALSE)
   }
 
-  list(data = ox_item_data(parsed_xml),
-       metadata = list(global_vars = ox_global_vars(parsed_xml),
-                       event_def = ox_event_def(parsed_xml),
-                       event_ref = ox_event_ref(parsed_xml),
-                       form_def = ox_form_def(parsed_xml),
-                       form_ref = ox_form_ref(parsed_xml),
-                       group_def = ox_group_def(parsed_xml),
-                       group_ref = ox_group_ref(parsed_xml),
-                       item_def = ox_item_def(parsed_xml),
-                       item_ref = ox_item_ref(parsed_xml),
-                       codelist = ox_codelist(parsed_xml),
-                       codelist_item = ox_codelist_item(parsed_xml),
-                       codelist_ref = ox_codelist_ref(parsed_xml),
-                       units = ox_measurement_units(parsed_xml),
-                       sites = ox_sites(parsed_xml),
-                       subjects = ox_subject_data(parsed_xml))) -> ox_obj
+  ox_obj <- list(data = ox_item_data(parsed_xml),
+                 metadata = ox_metadata(parsed_xml))
 
   # assign class ox
   class(ox_obj) <- c("ox", "list")
