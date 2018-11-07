e1d4beb6ea0cbd6f0927c3eb5a2f0dc3f953b7e1
Albert Cobos <acoboscarbo@gmail.com>
2018-11-07 09:26:52 +0100

    Documentation.

--- R/summary.ox.R
+++ R/summary.ox.R
@@ -1,5 +1,40 @@
-# S3 method for ox objects
-
+#' Summary for \code{ox} objects
+#'
+#' Produces a summary of an \code{ox} object, including minimal information on
+#' datapoints, subjects, items, groups, forms and events.
+#'
+#' @param ox_obj An object of class \code{ox}, as returned by \code{ox()}.
+#'
+#' @return A list with the following components:
+#'
+#' \itemize{
+#'   \item \code{numbers}: numbers of datapoints, subjects, sites, events, forms,
+#' groups, and items, in  \code{ox_obj$data}.
+#'   \item \code{events}: unique values of \code{event_oid} in \code{ox_obj$data}.
+#'   \item \code{forms}: unique values of \code{form_oid} in \code{ox_obj$data}.
+#'   \item \code{groups}: unique values of \code{group_oid} in \code{ox_obj$data}.
+#'   \item \code{assessments}: table of \code{event_oid} by \code{group_oid}, in
+#'   \code{ox_obj$data}, Shows the number of datapoints (rows in
+#' \code{ox_obj$data}) per event-group combination.
+#' }
+#'
+#' @export
+#'
+#' @examples
+#' # The example odm1.3 xml file address
+#' my_file <- system.file("extdata",
+#'                        "odm1.3_clinical_ext_example.xml",
+#'                        package = "ox",
+#'                        mustWork = TRUE)
+#'
+#' # Parsing the xml file
+#' library(XML)
+#' doc <- xmlParse(my_file)
+#'
+#' # Creating ox object and getting summary info
+#' my_ox_obj <- ox(doc)
+#' summary(my_ox_obj)
+#'
 summary.ox <- function (ox_obj) {
 
   list(numbers = c(datapoints = nrow(d$data),
