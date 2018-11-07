80f02bb2d0a383204c2e6fa2418d3d94f6b17f5a
Albert Cobos <acoboscarbo@gmail.com>
2018-11-07 10:24:49 +0100

    Remove rubish.

--- R/ox_item_data.R
+++ R/ox_item_data.R
@@ -30,58 +30,11 @@ ox_item_data <- function(parsed_xml) {
     stop("parsed_xml should be an object of class XMLInternalDocument", call. = FALSE)
   }
 
-<<<<<<< HEAD
-  item_data <- bind_rows(lapply(xpathApply(parsed_xml,
-                                           "//ns:ItemData",
-                                           namespaces = ox_alias_default_ns(parsed_xml),
-                                           fun=xmlAncestors,
-                                           xmlAttrs),
-                                data.frame,
-                                stringsAsFactors=FALSE)) %>%
-    select(study_oid = StudyOID,
-           metadata_version_oid = MetaDataVersionOID,
-           subject_key = SubjectKey,
-           subject_id = StudySubjectID,
-           subject_status = Status,
-           event_oid = StudyEventOID,
-           form_oid = FormOID,
-           form_version = Version,
-           form_status = Status.1,
-           group_oid = ItemGroupOID,
-           group_repeat_key = ItemGroupRepeatKey,
-           trasaction_type = TransactionType,
-           item_oid = ItemOID,
-           value = Value)
-||||||| merged common ancestors
-  item_data <- bind_rows(lapply(xpathApply(parsed_xml,
-                                           "//ns:ItemData",
-                                           namespaces = ox_alias_default_ns(parsed_xml),
-                                           fun=xmlAncestors,
-                                           xmlAttrs),
-                                data.frame,
-                                stringsAsFactors=FALSE)) %>%
-    select(study_oid = StudyOID,
-           metadata_version_oid = MetaDataVersionOID,
-           subject_key = SubjectKey,
-           subject_id = StudySubjectID,
-           subject_status = Status,
-           sex = Sex,
-           event_oid = StudyEventOID,
-           start_date = StartDate,
-           form_oid = FormOID,
-           form_version = Version,
-           form_status = Status.1,
-           group_oid = ItemGroupOID,
-           group_repeat_key = ItemGroupRepeatKey,
-           trasaction_type = TransactionType,
-           item_oid = ItemOID,
-           value = Value)
-=======
   message("Getting ItemData nodes...")
   nodes <- XML::xpathApply(parsed_xml,
                            "//ns:ItemData",
                            namespaces = .ns_alias(parsed_xml, "ns"))
->>>>>>> nicer_group_df
+
 
   message("Extracting data from ItemData nodes...")
 
