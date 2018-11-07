eedd96132caf1883ae0604271fb2638137a0be94
Albert Cobos <acoboscarbo@gmail.com>
2018-11-07 15:04:49 +0100

    New fun to get file info.

---
+++ R/ox_file_info.R
@@ -0,0 +1,35 @@
+#' File info in a list
+#'
+#' Returns a list with information on the .xml file, from a parsed OpenClinica
+#' odm1.3 .xml export file.
+#'
+#' @param parsed_xml An object of class \code{XMLInternalDocument}, as returned
+#' by \code{XML::xmlParse()}.
+#'
+#' @return A list.
+#' @export
+#'
+#' @examples
+#' # The example odm1.3 xml file address
+#' my_file <- system.file("extdata",
+#'                        "odm1.3_clinical_ext_example.xml",
+#'                        package = "ox",
+#'                        mustWork = TRUE)
+#'
+#' # Parsing the xml file
+#' library(XML)
+#' doc <- xmlParse(my_file)
+#'
+#' # File info in a list
+#' file_info <- ox_file_info(doc)
+#' file_info
+ox_file_info <- function (parsed_xml) {
+
+  if (! "XMLInternalDocument" %in% class(parsed_xml)) {
+    stop("parsed_xml should be an object of class XMLInternalDocument", call. = FALSE)
+  }
+
+  as.list(XML::xmlAttrs(xmlRoot(parsed_xml)))
+}
+
+
