52c433a5bfc15400cc6950f10eb81f4f527dd3de
Albert Cobos <acoboscarbo@gmail.com>
2018-11-09 09:48:45 +0100

    Help file for package.

---
+++ R/ox.R
@@ -0,0 +1,22 @@
+#' ox: A package for getting OpenClinica data into R
+#'
+#' The ox package provides three categories of functions:
+#'
+#' - functions to create `ox_all` objects and work with them
+#'
+#' - functions to get data from a parsed xml file
+#'
+#' - functions to get metadata from a parsed xml file
+#'
+#' @section create `ox_all` objects and work with them:
+#' These ox functions ...
+#'
+#' @section get data from a parsed xml file:
+#' These ox functions ...
+#'
+#' @section get metadata from a parsed xml file:
+#' These ox functions ...
+#'
+#' @docType package
+#' @name ox
+NULL
