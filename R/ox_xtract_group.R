b1fccd30ef5fc315b64076b4ad20c1890d707279
Albert Cobos <acoboscarbo@gmail.com>
2018-11-10 17:10:11 +0100

    Dealing with vars not always present.

--- R/ox_xtract_group.R
+++ R/ox_xtract_group.R
@@ -83,13 +83,13 @@ ox_xtract_group <- function (ox_all_obj, group,
     dplyr::filter(group_oid == group) %>%
     dplyr::select(study_oid,
                   subject_key,
-                  # subject_id,
+                  contains("subject_id"),
                   event_oid,
-                  event_repeat_key,
+                  contains("event_repeat_key"),    # because NOT always present!
                   form_oid,
                   group_oid,
                   item_oid,
-                  group_repeat_key,
+                  contains("group_repeat_key"),    # just in case
                   value) %>%
     tidyr::spread(item_oid , value) -> k
 
