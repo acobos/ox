2c6041d4e71c2e8aca496f7392e70cf40426ca00
Albert Cobos <acoboscarbo@gmail.com>
2018-11-10 17:06:50 +0100

    A function to simplify all others.

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,24 @@
   x[alias]
 }
 
+# attributes of node and ancestors
+.attrs_node_and_ancestors <- function (parsed_xml, node_name, ns_alias = "ns") {
+
+  if (! "XMLInternalDocument" %in% class(parsed_xml)) {
+    stop("parsed_xml should be an object of class XMLInternalDocument", call. = FALSE)
+  }
+
+  # validation of no_name PENDING !!
+
+  # return
+  dplyr::bind_rows(
+    lapply(
+      XML::xpathApply(parsed_xml,
+                      paste0("//", ns_alias, ":", node_name), # "//ns:StudyEventDef",
+                      namespaces = .ns_alias(parsed_xml = parsed_xml,
+                                             alias = ns_alias),
+                      fun = XML::xmlAncestors,
+                      XML::xmlAttrs),
+      data.frame, stringsAsFactors=FALSE))
+
+}
